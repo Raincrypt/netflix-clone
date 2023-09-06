2b337f2409b8505b882cfc1d4c0be0a2b2389aab
Mousom <sarkarmousom8@gmail.com>
2023-09-06 18:37:15 +0530

    Initial Build

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -32,33 +32,33 @@ const SavedShows = () => {
   };
 
   return (
-    <section className="absolute top-[20%] p-4 md:p-8">
-      <h1 className="font-bold text-3xl md:text-5xl">My Shows</h1>
+    <section className="p-4 md:p-8">
+      <h1 className="font-bold text-3xl mb-6 md:text-5xl">Favourites</h1>
 
       <div>
-        <h2 className="text-white font-bold py-6 md:text-xl">Favourites</h2>
-        <div className="flex items-center group">
-          <div id={`slider`} className="relative w-full h-full">
-            {movies.map((movie) => {
-              return (
-                <article key={movie.id} className="relative inline-block p-2 cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px]">
-                  <div className="absolute w-full h-full top-0 left-0 hover:bg-black/80 opacity-0 hover:opacity-100 text-white group">
-                    <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-                      {movie.title}
-                    </p>
-                  </div>
-                  <img
-                    className="w-full h-auto block"
-                    src={`${imageBaseURL}/${movie.image}`}
-                    alt={movie.title}
-                  />
-                  <p onClick={() => deleteMovie(movie.id)}>
-                    <FaHeart className="absolute top-4 left-4 text-red-600" />
+        <div className="flex items-center flex-wrap">
+          {movies.map((movie) => {
+            return (
+              <article
+                key={movie.id}
+                className="relative inline-block p-2 cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px]"
+              >
+                <div className="absolute w-full h-full top-0 left-0 hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                  <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+                    {movie.title}
                   </p>
-                </article>
-              );
-            })}
-          </div>
+                </div>
+                <img
+                  className="w-full h-auto block"
+                  src={`${imageBaseURL}/${movie.image}`}
+                  alt={movie.title}
+                />
+                <p onClick={() => deleteMovie(movie.id)}>
+                  <FaHeart className="absolute top-4 left-4 text-red-600" />
+                </p>
+              </article>
+            );
+          })}
         </div>
       </div>
     </section>
